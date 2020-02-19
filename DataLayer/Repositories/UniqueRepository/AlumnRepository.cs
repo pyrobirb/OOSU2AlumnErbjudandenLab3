a854d7e0dd339c4aaad0aa04499ae0995db7a9c6
Phyrexiian <s182240@student.hb.se>
2020-02-19 22:50:52 +0100

    unitofwork,repositories

---
+++ DataLayer/Repositories/UniqueRepository/AlumnRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessEntites.Models;
+using DataLayer.Contexts;
+using DataLayer.Repositories.UniqueRepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories.UniqueRepository
+{
+    public class AlumnRepository : Repository<Alumn>, IAlumnRepository
+    {
+        public AlumnRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ DataLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Repositories.UniqueRepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IAlumnRepository AlumnRepository { get; set; }
+
+        void Commit();
+    }
+}
