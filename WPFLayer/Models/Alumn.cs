025dfd29c3ee84d23cab6083d596ac374d72dacc
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-03-09 23:45:54 +0100

    CreateAlumnAcc window

---
+++ WPFLayer/Models/Alumn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLayer.Models
+{
+    public class Alumn
+    {
+        public int AnvändarID { get; set; }
+        public string Användarnamn { get; set; }
+        public string Lösenord { get; set; }
+
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+
+    }
+}
