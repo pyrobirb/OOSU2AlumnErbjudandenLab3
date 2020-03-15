4a5b5ddfd0591a2f26c110be03a235430eedec42
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-03-15 11:34:50 +0100

    Interface ClassDiagram FIx

--- DataLayer/UnitOfWork/IUnitOfWork.cs
+++ DataLayer/UnitOfWork/IUnitOfWork.cs
@@ -10,6 +10,12 @@ namespace DataLayer.UnitOfWork
     public interface IUnitOfWork
     {
         IAlumnRepository AlumnRepository { get; set; }
+        IPersonalRepository PersonalRepository { get; set; }
+        IAktivitetRepository AktivitetRepository { get; set; }
+        IInformationsutskickRepository InformationsutskickRepository { get; set; }
+        IKompetensRepository KompetensRepository { get; set; }
+        IProgramRepository ProgramRepository { get; set; }
+        IMaillistRepository MaillistaRepository { get; set; }
 
         void Commit();
     }
