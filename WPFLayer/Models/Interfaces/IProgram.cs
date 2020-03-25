8b95c29aa9012d48265b6e325b30385936067ebb
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-03-25 12:47:06 +0100

    IProgram (IPersonal förra commit)

---
+++ WPFLayer/Models/Interfaces/IProgram.cs
@@ -0,0 +1,22 @@
+﻿using BusinessEntites.Models.Junction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLayer.Models.Interfaces
+{
+    public interface IProgram
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+        void Changed([CallerMemberName] String propertyName = "");
+        
+        ICollection<AlumnProgramDTO> AlumnProgram { get; set; }
+        int ProgramID { get; set; }
+        string Namn { get; set; }
+
+    }
+}
