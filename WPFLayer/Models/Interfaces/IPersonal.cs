b8f26d46fbcd11dfc5f1679ce4527ba0594effb1
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-03-25 12:45:06 +0100

    Interface IProgram

---
+++ WPFLayer/Models/Interfaces/IPersonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLayer.Models.Interfaces
+{
+    public interface IPersonal
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+        void Changed([CallerMemberName] String propertyName = "");
+        
+        int PersonalID { get; set; }
+        string Användarnamn { get; set; }
+        string Lösenord { get; set; }
+        string Förnamn { get; set; }
+        string Efternamn { get; set; }
+
+    }
+}
--- WPFLayer/Models/Personal.cs
+++ WPFLayer/Models/Personal.cs
@@ -1,24 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using WPFLayer.Models.Interfaces;
 
 namespace WPFLayer.Models
 {
     [DataContract]
-    public class Personal
+    public class Personal : INotifyPropertyChanged, IPersonal
     {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Changed([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         [DataMember]
-        public int PersonalID { get; set; }
+        private int personalID;
         [DataMember]
-        public string Användarnamn { get; set; }
+        private string användarnamn;
         [DataMember]
-        public string Lösenord { get; set; }
+        private string lösenord;
         [DataMember]
-        public string Förnamn { get; set; }
+        private string förnamn;
         [DataMember]
-        public string Efternamn { get; set; }
+        private string efternamn;
+
+        public int PersonalID
+        {
+            get { return personalID; }
+            set { personalID = value; }
+        }
+        public string Användarnamn
+        {
+            get { return användarnamn; }
+            set { användarnamn = value; }
+        }
+        public string Lösenord
+        {
+            get { return lösenord; }
+            set { lösenord = value; }
+        }
+        public string Förnamn
+        {
+            get { return förnamn; }
+            set { förnamn = value; }
+        }
+
+        public string Efternamn
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
     }
 }
