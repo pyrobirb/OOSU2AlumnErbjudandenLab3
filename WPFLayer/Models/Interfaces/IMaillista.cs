e4ee946381045f036b2f2b91b39345729231ea5b
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-03-25 12:38:02 +0100

    Interface IMaillista

---
+++ WPFLayer/Models/Interfaces/IMaillista.cs
@@ -0,0 +1,23 @@
+﻿using BusinessEntites.Models.Junction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLayer.Models.Interfaces
+{
+    public interface IMaillista
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+        void Changed([CallerMemberName] String propertyName = "");
+        
+        ICollection<AlumnMaillistaDTO> AlumnMaillistor { get; set; }
+
+        int MaillistaID { get; set; }
+        string MaillistaNamn { get; set; }
+
+    }
+}
